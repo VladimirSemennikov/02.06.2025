0932e1349626b601549b4c6b14ded30b7ed3bbdf
VladimirSemennikov <mectb1992@gmail.com>
2025-06-02 20:47:41 +0400

    das

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-print(type(1))
-print(type(1.1))
-print(type("1"))
-print(type([1]))
-print(type((1,)))
-print(type({1}))
-print(type({1:1}))
-print(type(min))
+# print(type(1))
+# print(type(1.1))
+# print(type("1"))
+# print(type([1]))
+# print(type((1,)))
+# print(type({1}))
+# print(type({1:1}))
+# print(type(min))
+
+""" Свойства и методы классов
+
+    Свойства - переменные которые находятся внутри класса
+    Метод - функции которые находятся внутри класса
+    
+"""
+
+# class Apple:
+#     form = "Круглая"
+#     color = "Цвет"
+#     taste = "Сладкий"
+#
+# print(Apple.form)
+# print(Apple.color)
+# print(Apple.taste)
+
+# class Car:
+#     color = "blue"
+#     marka = "Lada"
+# print(Car.color)
+# print(Car.marka)
+# print(f"Моя машина {Car.color} цвета и марки {Car.marka}")
+
+
